a557c2f659ad38ce3bdf98df56bc814ed43ef20a
Philopater Phoenix <Philopater.phoenix@gmail.com>
2023-01-29 21:27:35 -0600

    fix

--- app/ChatInput.tsx
+++ app/ChatInput.tsx
@@ -12,68 +12,69 @@ type Props = {
 };
 
 function ChatInput({ session }: Props) {
-  const [input, setInput] = useState("");
-  const { data: messages, error, mutate } = useSWR("/api/getMessages", fetcher);
-
-  const addMessage = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (!input || !session) return;
-
-    const messageToSend = input;
-
-    setInput("");
-
-    const id = uuid();
-
-    const message: Message = {
-      id,
-      message: messageToSend,
-      // @ts-ignore
-      username: session?.user?.name!,
-      created_at: Date.now(),
-      // @ts-ignore
-      email: session?.user?.email!,
-      // @ts-ignore
-      profilePic: session?.user?.image!,
-    };
-
-    const uploadMessageToUpStash = async () => {
-      const data = await fetch("/api/addMessage", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ message }),
-      }).then((res) => res.json());
-
-      return [data.message, ...messages!];
-    };
-
-    await mutate(uploadMessageToUpStash, {
-      optimisticData: [message, ...messages!],
-      rollbackOnError: true,
-    });
-  };
+  // const [input, setInput] = useState("");
+  // const { data: messages, error, mutate } = useSWR("/api/getMessages", fetcher);
+  //
+  // const addMessage = async (e: FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //
+  //   if (!input || !session) return;
+  //
+  //   const messageToSend = input;
+  //
+  //   setInput("");
+  //
+  //   const id = uuid();
+  //
+  //   const message: Message = {
+  //     id,
+  //     message: messageToSend,
+  //     // @ts-ignore
+  //     username: session?.user?.name!,
+  //     created_at: Date.now(),
+  //     // @ts-ignore
+  //     email: session?.user?.email!,
+  //     // @ts-ignore
+  //     profilePic: session?.user?.image!,
+  //   };
+  //
+  //   const uploadMessageToUpStash = async () => {
+  //     const data = await fetch("/api/addMessage", {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify({ message }),
+  //     }).then((res) => res.json());
+  //
+  //     return [data.message, ...messages!];
+  //   };
+  //
+  //   await mutate(uploadMessageToUpStash, {
+  //     optimisticData: [message, ...messages!],
+  //     rollbackOnError: true,
+  //   });
+  // };
   return (
-    <form
-      onSubmit={addMessage}
-      className="fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-t border-gray-100 bg-white"
-    >
-      <input
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="Enter message here..."
-        disabled={!session}
-        type="text"
-        className="flex-1 rounded border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent px-5 py-3 disabled:opacity-50 disabled:cursor-not-allowed"
-      />
-      <button
-        type="submit"
-        disabled={!input}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed"
-      >
-        Send
-      </button>
-    </form>
+    // <form
+    //   onSubmit={addMessage}
+    //   className="fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-t border-gray-100 bg-white"
+    // >
+    //   <input
+    //     value={input}
+    //     onChange={(e) => setInput(e.target.value)}
+    //     placeholder="Enter message here..."
+    //     disabled={!session}
+    //     type="text"
+    //     className="flex-1 rounded border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent px-5 py-3 disabled:opacity-50 disabled:cursor-not-allowed"
+    //   />
+    //   <button
+    //     type="submit"
+    //     disabled={!input}
+    //     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed"
+    //   >
+    //     Send
+    //   </button>
+    // </form>
+    <div>hi</div>
   );
 }
 
