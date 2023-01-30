32307b0c097fbecb81164f050d23e9a777a2b560
Philopater Phoenix <Philopater.phoenix@gmail.com>
2023-01-29 21:23:03 -0600

    fix

--- app/MessageList.tsx
+++ app/MessageList.tsx
@@ -12,33 +12,33 @@ type Props = {
 };
 
 function MessageList({ initialMessages }: Props) {
-  const {
-    data: messages,
-    error,
-    mutate,
-  } = useSWR<Message[]>("/api/getMessages", fetcher);
+  // const {
+  //   data: messages,
+  //   error,
+  //   mutate,
+  // } = useSWR<Message[]>("/api/getMessages", fetcher);
 
-  useEffect(() => {
-    const channel = clientPusher.subscribe("messages");
-
-    channel.bind("new-message", async (data: Message) => {
-      if (messages?.find((message) => message.id === data.id)) return;
-
-      if (!messages) {
-        await mutate(fetcher);
-      } else {
-        await mutate(fetcher, {
-          optimisticData: [data, ...messages!],
-          rollbackOnError: true,
-        });
-      }
-    });
-
-    return () => {
-      channel.unbind_all();
-      channel.unsubscribe();
-    };
-  }, [messages, mutate, clientPusher]);
+  // useEffect(() => {
+  //   const channel = clientPusher.subscribe("messages");
+  //
+  //   channel.bind("new-message", async (data: Message) => {
+  //     if (messages?.find((message) => message.id === data.id)) return;
+  //
+  //     if (!messages) {
+  //       await mutate(fetcher);
+  //     } else {
+  //       await mutate(fetcher, {
+  //         optimisticData: [data, ...messages!],
+  //         rollbackOnError: true,
+  //       });
+  //     }
+  //   });
+  //
+  //   return () => {
+  //     channel.unbind_all();
+  //     channel.unsubscribe();
+  //   };
+  // }, [messages, mutate, clientPusher]);
 
   return (
     <div className="space-y-5 px-5 pt-8 pb-32 ">
