d4ab15e8e6817899755c1b87d8707bf3e758b4d7
Felopater Melika <FelopaterMelika@gmail.com>
2023-08-05 13:22:40 -0500

    fix

--- app/ChatInput.tsx
+++ app/ChatInput.tsx
@@ -60,8 +60,28 @@ function ChatInput({ session }: Props) {
     mutation.mutate(message);
   };
 
-  // rest of the component
+  return (
+      <form
+          onSubmit={sendMessage}
+          className='fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-t border-gray-100 bg-gray-900'
+      >
+        <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder='Enter message here...'
+            disabled={!session}
+            type='text'
+            className='flex-1 rounded border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent px-5 py-3 disabled:opacity-50 disabled:cursor-not-allowed'
+        />
+        <button
+            type='submit'
+            disabled={!input}
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed'
+        >
+          Send
+        </button>
+      </form>
+  );
 }
-
 
 export default ChatInput;
